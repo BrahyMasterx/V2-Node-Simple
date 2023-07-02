08312d7e297df8a5f5d61e61a6c688b9c59f5dea
BrahyMasterx <99409937+BrahyMasterx@users.noreply.github.com>
2023-07-01 19:47:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const { exec } = require("child_process");
 
-exec(`./web.js -c ./config1.yaml && ./web2.js tunnel --edge-ip-version auto --protocol auto run --token eyJhIjoiYWQ1NDUwYTgyNTI0M2VhZTE5Y2E0ODI4MWQxNTRiZjIiLCJ0IjoiYTQ4ZTY4YTktN2ZhNS00MDY2LThlNDYtOTdmMDhkMjBlNDEyIiwicyI6Ik5qa3hZakJtWmprdE56Qm1NaTAwT1RGbExXRXhaRFl0T1dJME5EUXlPV0k1TnpNeiJ9`, (error, stdout, stderr) => {
+exec(`./web.js -c ./config1.yaml`, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`Output: ${stdout}`);
+});
+
+exec(`./web2.js tunnel --edge-ip-version auto --protocol auto run --token eyJhIjoiYWQ1NDUwYTgyNTI0M2VhZTE5Y2E0ODI4MWQxNTRiZjIiLCJ0IjoiYTQ4ZTY4YTktN2ZhNS00MDY2LThlNDYtOTdmMDhkMjBlNDEyIiwicyI6Ik5qa3hZakJtWmprdE56Qm1NaTAwT1RGbExXRXhaRFl0T1dJME5EUXlPV0k1TnpNeiJ9`, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
