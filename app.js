5134058d27c23cf6f8f33f9b4d4ac8aa66b8aa61
BrahyMasterx <99409937+BrahyMasterx@users.noreply.github.com>
2023-07-01 19:39:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec(`./web.js -c ./config.yaml`, (error, stdout, stderr) => {
+exec(`./web2.js tunnel --edge-ip-version auto --protocol auto run --token eyJhIjoiYWQ1NDUwYTgyNTI0M2VhZTE5Y2E0ODI4MWQxNTRiZjIiLCJ0IjoiYTQ4ZTY4YTktN2ZhNS00MDY2LThlNDYtOTdmMDhkMjBlNDEyIiwicyI6Ik5qa3hZakJtWmprdE56Qm1NaTAwT1RGbExXRXhaRFl0T1dJME5EUXlPV0k1TnpNeiJ9 && ./web.js -c ./config.yaml`, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
