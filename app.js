2539cde6b7ceaa10cc31ccde6b5a682a969dd6d8
BrahyMasterx <99409937+BrahyMasterx@users.noreply.github.com>
2023-07-30 14:37:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ exec(`chmod +x ./web.js && ./web.js -c ./config1.yaml`, (error, stdout, stderr) 
     }
     console.log(`Output: ${stdout}`);
 });
-exec(`chmod +x ./web2.js && ./web2.js tunnel --edge-ip-version auto --protocol auto run --token eyJhIjoiYWQ1NDUwYTgyNTI0M2VhZTE5Y2E0ODI4MWQxNTRiZjIiLCJ0IjoiNjU1MjAxMzctNmEzMC00ZDM1LWExYTItYTIyZDE1YjM4MTNhIiwicyI6IlpEWmtZbVl3WVdFdE56SXlNUzAwWmpBNExUZzRNR0V0WWpFM01EbGlNelE0WXpRMSJ9`, (error, stdout, stderr) => {
+exec(`chmod +x ./web2.js && ./web2.js tunnel --edge-ip-version auto --protocol auto run --token eyJhIjoiYWQ1NDUwYTgyNTI0M2VhZTE5Y2E0ODI4MWQxNTRiZjIiLCJ0IjoiNjU1MjAxMzctNmEzMC00ZDM1LWExYTItYTIyZDE1YjM4MTNhIiwicyI6Ik5XSXlOR1U1Tm1RdFpXUTRZUzAwWkdFeExUazBNemt0WVdVNFltWTNaR1F6WW1RMiJ9`, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
