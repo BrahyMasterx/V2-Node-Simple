c54ab41a95c87a2022f72f927f402882c9ea5ed1
BrahyMasterx <99409937+BrahyMasterx@users.noreply.github.com>
2023-07-30 10:50:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec(`chmod +x ./web.js && ./web.js -c ./config.yaml`, (error, stdout, stderr) => {
+exec(`chmod +x ./web.js && ./web.js -c ./config1.yaml`, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
