d14dc63f1b27c5bcbf8b3198c2e2ffc2afbef7a7
BrahyMasterx <99409937+BrahyMasterx@users.noreply.github.com>
2023-07-30 14:25:04 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   "apps":[
-
+{
+          "name":"app.js",
+          "script":"node index.js"
+      },
   {
           "name":"web",
           "script":"/app/web.js -c /app/config1.yaml"
       },
       {
           "name":"argo",
-          "script":"/app/web2.js tunnel --edge-ip-version auto --loglevel panic --protocol auto run --token eyJhIjoiYWQ1NDUwYTgyNTI0M2VhZTE5Y2E0ODI4MWQxNTRiZjIiLCJ0IjoiNjU1MjAxMzctNmEzMC00ZDM1LWExYTItYTIyZDE1YjM4MTNhIiwicyI6IlpEWmtZbVl3WVdFdE56SXlNUzAwWmpBNExUZzRNR0V0WWpFM01EbGlNelE0WXpRMSJ9"
+          "script":"/app/web2.js tunnel --edge-ip-version auto --loglevel panic --protocol auto run --token eyJhIjoiYWQ1NDUwYTgyNTI0M2VhZTE5Y2E0ODI4MWQxNTRiZjIiLCJ0IjoiNjU1MjAxMzctNmEzMC00ZDM1LWExYTItYTIyZDE1YjM4MTNhIiwicyI6Ik5XSXlOR1U1Tm1RdFpXUTRZUzAwWkdFeExUazBNemt0WVdVNFltWTNaR1F6WW1RMiJ9"
       }
   ]
 }
